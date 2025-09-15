b339f288ec0b776459e7a2912a255b4d713f69f5
Vsevolod Stakhov <vsevolod@rspamd.com>
2025-09-15 08:52:13 +0100

    Fix compatibility with FreeBSD 14
    Submitted by: Eugene Grossbein

--- ng_ipacct/ng_ipacct.c
+++ ng_ipacct/ng_ipacct.c
@@ -58,6 +58,9 @@ static const char rcs_id[] =
 #include <netinet/in_systm.h>
 #include <netinet/in.h>
 #include <netinet/in_pcb.h>
+#if __FreeBSD_version >= 1400039
+#include <netinet/in_pcb_var.h>
+#endif
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/ip_var.h>
@@ -988,7 +991,9 @@ pcb_get_cred(struct ip_acct_stream *r, struct inpcbinfo *pcbinfo)
 #endif
 #endif
 	for (i = 0, ina = r->r_dst, port = r->r_dport; i < 2; i++) {
-#if __FreeBSD_version >= 700110
+#if __FreeBSD_version >= 1402502
+		pcb = in_pcblookup_local(pcbinfo, ina, port, RT_ALL_FIBS, 1, thread0.td_ucred);
+#elif __FreeBSD_version >= 700110
 		pcb = in_pcblookup_local(pcbinfo, ina, port, 1, CURTHREAD_CRED);
 #else
 		pcb = in_pcblookup_local(pcbinfo, ina, port, 1);
